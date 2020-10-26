62aa3170d233b25d45e93b79b8dc5cf9640dba5f
IvanUlloa098 <iulloa@est.ups.edu.ec>
2020-10-26 17:59:00 -0500

    Paginacion y API en funcionamiento

--- api_access.js
+++ api_access.js
@@ -1,4 +1,13 @@
-var data = null;
+var rawdata = null;
+var details = "";
+
+var state = {
+    'querySet': null,
+
+    'page': 1,
+    'rows': 5,
+    'window': 5,
+}
 
 function searchAPI() {
     
@@ -22,25 +31,12 @@ function searchAPI() {
             //alert("llegue "+this.status);
             if (this.readyState == 4 && this.status == 200) {
                 //alert("here");
-                data = JSON.parse(this.responseText);
-                details = "";
-                //console.log(data);
-                //console.log(data.meals[0].strMeal);
-                //console.log(data.meal[0]);    
-
-                for(var i=0; i < data.meals.length; i++){
-                    console.log(data.meals[i].strMeal);
-                    details += "<tr>"+
-                        "<td>" + data.meals[i].strMeal + "</td>" +
-                        "<td>" + data.meals[i].strDrinkAlternate + "</td>" +
-                        "<td>" + data.meals[i].strCategory + "</td>" +
-                        "<td>" + data.meals[i].strArea + "</td>" +
-                        "<td>" + data.meals[i].strInstructions + "</td>" +
-                        "<td> <img src=" + data.meals[i].strMealThumb + " width=\"100\" height=\"100\"> </td>" +
-                        "</tr>";
-                }
-                console.log(details);
-                document.getElementById("detailsTable").innerHTML = details;
+                rawdata = JSON.parse(this.responseText);
+                state.querySet = rawdata.meals;
+                buildTable();
+                
+                //console.log(details);
+                //document.getElementById("detailsTable").innerHTML = details;
 
             }
         };
@@ -51,6 +47,87 @@ function searchAPI() {
 
 }
 
-function readJSON() {
+function pagination(querySet, page, rows) {
+
+    var trimStart = (page - 1) * rows
+    var trimEnd = trimStart + rows
+    console.log(querySet.length);
+    var trimmedData = querySet.slice(trimStart, trimEnd)
+
+    var pages = Math.round(querySet.length / rows);
+
+    return {
+        'querySet': trimmedData,
+        'pages': pages,
+    }
+}
+
+function pageButtons(pages) {
+    var wrapper = document.getElementById('pagination-wrapper')
+
+    wrapper.innerHTML = ``
+	//console.log('Pages:', pages)
+
+    var maxLeft = (state.page - Math.floor(state.window / 2))
+    var maxRight = (state.page + Math.floor(state.window / 2))
+
+    if (maxLeft < 1) {
+        maxLeft = 1
+        maxRight = state.window
+    }
+
+    if (maxRight > pages) {
+        maxLeft = pages - (state.window - 1)
+        
+        if (maxLeft < 1){
+        	maxLeft = 1
+        }
+        maxRight = pages
+    }
     
+    
+
+    for (var page = maxLeft; page <= maxRight; page++) {
+    	wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-info">${page}</button>`
+    }
+
+    if (state.page != 1) {
+        wrapper.innerHTML = `<button value=${1} class="page btn btn-sm btn-info">&#171; First</button>` + wrapper.innerHTML
+    }
+
+    if (state.page != pages) {
+        wrapper.innerHTML += `<button value=${pages} class="page btn btn-sm btn-info">Last &#187;</button>`
+    }
+
+    $('.page').on('click', function() {
+        $('#detailsTable').empty()
+
+        state.page = Number($(this).val())
+
+        buildTable()
+    })
+
+}
+
+
+function buildTable() {
+    var table = $('#detailsTable')
+
+    var data = pagination(state.querySet, state.page, state.rows)
+    console.log(data)
+    var myList = data.querySet
+
+    for (var i = 1 in myList) {
+        //Keep in mind we are using "Template Litterals to create rows"
+        var row = `<tr>
+                  <td>${myList[i].strMeal}</td>
+                  <td>${myList[i].strCategory}</td>
+                  <td>${myList[i].strArea}</td>
+                  <td>${myList[i].strInstructions}</td>
+                  <td class=\"tg-0pky\"> <img src=${myList[i].strMealThumb} width=\"200\" height=\"200\"> </td>
+                  `
+        table.append(row)
+    }
+
+    pageButtons(data.pages)
 }
